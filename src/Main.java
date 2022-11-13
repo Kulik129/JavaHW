c38005d5a84d32049039986a3ecf5901547a30c6
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2022-11-13 20:51:06 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,45 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        pi();
-    }
-
-    public static void pi(){
         ArrayList<String> pp = new ArrayList<>();
-
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите Фамилию");
-        String su = in.nextLine();
-        System.out.println("Введите Имя");
-        String na = in.nextLine();
-        System.out.println("Введите Отчество");
-        String ot = in.nextLine();
-        System.out.println("Введите Дату рождения в формате дд.мм.гггг");
-        String bi = in.nextLine();
+        System.out.println("Введите ФИО");
+        String name = in.nextLine();
+        System.out.println("Введите дату рождения в формате дд.мм.гггг");
+        String date = in.nextLine();
         System.out.println("Введите пол");
-        String po = in.next();
-        System.out.println("Продолжите свой номер телефона без +79");
-        if(in.hasNextInt()){
-            Integer num = in.nextInt();
-            pp.add(num.toString());
-            pp.add(su);
-            pp.add(na);
-            pp.add(ot);
-            pp.add(bi);
-            pp.add(po);
-        } else {
-            System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+        String gender = in.nextLine();
+        try {
+            System.out.println("Введите номер телефона без +79");
+            Integer numbers = in.nextInt();
+            System.out.println();
+            pp.add(name);
+            pp.add(date);
+            pp.add(gender);
+            pp.add(numbers.toString());
+        } catch (InputMismatchException e){
+            System.out.println("Это не число");
         }
-
-        for(String s : pp){
-            if(s.isEmpty()){
-                System.out.println("Строка не заполнена");
-            } else {
-                System.out.println(s);
+        for (String s : pp){
+            boolean a = s.isEmpty();
+            if(a == true){
+                System.out.println("Одна из строк пуста");
+            }else {
+                try (FileWriter writer = new FileWriter(name+".txt", false))
+                {
+                    ArrayList<String> text = pp;
+                    writer.write(String.valueOf(text));
+                    writer.append('\n');
+                    writer.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
-        try (FileWriter writer = new FileWriter(su, false))
-        {
-            ArrayList<String> text = pp;
-            writer.write(String.valueOf(text));
-            writer.append('\n');
-            writer.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
-
